ba5094993b7cde4f62d4de38259daaf0dae51aa2
Teja <67798221+kangScripter@users.noreply.github.com>
2024-10-11 20:34:54 +0530

    Update config.py

--- src/bot/config.py
+++ src/bot/config.py
@@ -1,6 +1,6 @@
 class Config(object):
     LOGGER = True
-    DATABASE_URI ='postgres://u65pvrolirbd5v:p349c544fdfd1d098897dd59ec7bce3140088ae7d2d1ffe8485e2c6e7da306dbb@c6sfjnr30ch74e.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dero9carbep2ih'
+    DATABASE_URI ='postgresql://u65pvrolirbd5v:p349c544fdfd1d098897dd59ec7bce3140088ae7d2d1ffe8485e2c6e7da306dbb@c6sfjnr30ch74e.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dero9carbep2ih'
     LOG_CHANNEL = "-1002005902455"
     SUDO_USERS=[5297549539]
     BOT_TOKEN='6030145995:AAGqawj9tdp6rAv6FSM6NonH6dbjKBX6rVc'
